4e6bce74c82840149e963121d324a4e9dcd5ab94
Viktorija Bezsazna <viktorija.bezsazna@gmail.com>
2017-10-14 19:33:27 -0400

    Rename function, fix code formatting

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -2,7 +2,7 @@ class DownloadController < ApplicationController
   def index
     results = FileReader.read_file
     results = ResultsFilter.filter(results, user_params)
-    send_data CsvGenerator.write_to_file(results),
+    send_data CsvGenerator.generate(results),
               filename: "results.csv",
               type: "text/csv"
   end
--- app/models/csv_generator.rb
+++ app/models/csv_generator.rb
@@ -1,11 +1,21 @@
 require 'csv'
 
 class CsvGenerator
-  def self.write_to_file(results)
+  def self.generate(results)
     CSV.generate do |csv|
-      csv << ["Player", "Team", "Pos", "Att", "Att/G", "Yds", "Avg", "Yds/G", "TD", "Lng", "1st", "1st%", "20+", "40+", "FUM"]
+      csv << [
+                "Player", "Team", "Pos", "Att",
+                "Att/G", "Yds", "Avg", "Yds/G",
+                "TD", "Lng", "1st", "1st%",
+                "20+", "40+", "FUM"
+              ]
       results.each do |row|
-        csv << [row["Player"], row["Team"], row["Pos"], row["Att"], row["Att/G"], row["Yds"], row["Avg"], row["Yds/G"], row["TD"], row["Lng"].to_s, row["1st"], row["1st%"], row["20+"], row["40+"], row["FUM"]]
+        csv << [
+                  row["Player"], row["Team"], row["Pos"], row["Att"],
+                  row["Att/G"], row["Yds"], row["Avg"], row["Yds/G"],
+                  row["TD"], row["Lng"].to_s, row["1st"], row["1st%"],
+                  row["20+"], row["40+"], row["FUM"]
+               ]
       end
     end
   end
