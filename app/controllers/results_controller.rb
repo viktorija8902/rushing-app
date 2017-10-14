12ef32859efca1b5f5959e980f5a290ef467520f
Viktorija Bezsazna <viktorija.bezsazna@gmail.com>
2017-10-14 12:47:58 -0400

    Implement filtering by player's name

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,15 @@
 class ResultsController < ApplicationController
   def index
     @results = FileReader.read_file
-    if params.has_key?(:sort_by)
-      @results = @results.sort_by { |result| result[params[:sort_by]].to_s }
+    if params.has_key?(:filter) && params.has_key?(:sort_by)
+      if params[:filter] != "" && params[:sort_by] != "null"
+        @results = @results.select {|result| result["Player"] == params[:filter] }
+        @results = @results.sort_by { |result| result[params[:sort_by]].to_s }
+      elsif params[:filter] != ""
+        @results = @results.select {|result| result["Player"] == params[:filter] }
+      elsif params[:sort_by] != "null"
+        @results = @results.sort_by { |result| result[params[:sort_by]].to_s }
+      end
       render partial: "results"
     end
   end
