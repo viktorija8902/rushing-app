d1b79ff1cca9d60e7046bfff2d5a475314d306d4
Viktorija Bezsazna <viktorija.bezsazna@gmail.com>
2017-10-14 14:30:13 -0400

    Permit only params: filter, sort_by, rename js file

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,9 +1,14 @@
 class DownloadController < ApplicationController
   def index
     results = FileReader.read_file
-    results = ResultsFilter.filter(results, params)
+    results = ResultsFilter.filter(results, user_params)
     send_data CsvGenerator.write_to_file(results),
               filename: "results.csv",
               type: "text/csv"
   end
+
+  private
+    def user_params
+      params.permit(:filter, :sort_by)
+    end
 end
